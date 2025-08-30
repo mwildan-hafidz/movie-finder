13e746d5060cb05b16e66378db170cae9bccaebe
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-08-30 18:09:02 +0800

    feature: make paragraph width change on medium screen.

--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -3,8 +3,13 @@ export default function TrendingMovies() {
     <section className="py-5 px-3 px-md-5">
       <div className="container">
         <h1 className="display-1">Movie Title</h1>
-        <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, officiis.</p>
-        <button className="btn btn-dark mt-md-5">Watch</button>
+        <p className="lead d-md-none">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta provident quia error qui inventore minus reiciendis quae dolor aut eius.
+        </p>
+        <p className="lead w-50 d-none d-md-block">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta provident quia error qui inventore minus reiciendis quae dolor aut eius.
+        </p>
+        <button className="btn btn-dark mt-3">Watch</button>
       </div>
     </section>
   );
