2881522ef6d3c9b72821890308c3c84372756e6a
mwildan-hafidz <m.wildan.hafidz.bjm@gmail.com>
2025-09-03 22:10:36 +0800

    feature: change navbar bg color when scrolled.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
-export default function Navbar() {
+import { useState, useEffect } from "react";
+
+function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      }
+      else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="navbar bg-transparent fixed-top" data-bs-theme="dark">
+    <nav
+      className={`navbar fixed-top ${scrolled ? 'bg-dark' : 'bg-transparent'}`}
+      data-bs-theme="dark"
+      style={{ transition: "background 200ms ease" }}
+    >
       <div className="container-fluid">
         <span className="navbar-brand h1 mb-0">Movie Finder</span>
       </div>
     </nav>
   );
 }
+
+export default Navbar;
